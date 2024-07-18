552a58411f4655fdd1ab8c291c84c8569afa7fc7
Emanuel <emednedezdelgado22>
2024-07-17 22:25:07 -0400

    added file

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
+    const innerTask = true;
+    // eslint-disable-next-line no-unused-vars
+    const innerTask2 = false;
   }
+
+  return [task, task2];
+}
