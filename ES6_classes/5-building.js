164ecf44ac450098ab6041ed68fda1369bcf5d07
Emanuel <emednedezdelgado22>
2024-07-19 19:37:00 -0400

    fixed file

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class');
-    }
     this._sqft = sqft;
+    if (this.constructor.name !== 'Building' && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
 
-  set sqft(value) {
-    this._sqft = value;
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 
   evacuationWarningMessage() {
