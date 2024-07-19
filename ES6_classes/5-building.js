3125239858511951850d87e4d8514e6d15272c55
Emanuel <emednedezdelgado22>
2024-07-19 19:33:48 -0400

    fixed file

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,8 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (new.target !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+    if (new.target !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
