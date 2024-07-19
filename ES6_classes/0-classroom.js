3d260d21e9bd9752c35201be425e3b2574e44f96
Emanuel <emednedezdelgado22>
2024-07-19 18:59:00 -0400

    fixed file

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,5 +1,13 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this.maxStudentsSize = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize;
+  }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+
+  set maxStudentsSize(size) {
+    this._maxStudentsSize = size;
   }
 }
