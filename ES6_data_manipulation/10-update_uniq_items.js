113fc2fb3bb3ecee244c48dffef4f8f4950c21d0
Emanuel <emednedezdelgado22>
2024-07-23 23:21:52 -0400

    changed file

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,15 @@
 function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-      throw Error('Cannot process');
-    }
-  
-    for (const [key] of map) {
-      if (map.get(key) === 1) map.set(key, 100);
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
       map.set(key, 100);
     }
-  
-    return map;
   }
-  
-  export default updateUniqueItems;
+
+  return map;
+}
+
+export default updateUniqueItems;
