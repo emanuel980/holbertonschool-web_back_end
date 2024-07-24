4fddd3e800fb60ea98fb469f5871f273ee337ec3
Emanuel <emednedezdelgado22>
2024-07-23 23:04:00 -0400

    changed file

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
 function getListStudents() {
-    const objects = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-  
-    return objects;
-  }
-  
-  export default getListStudents;
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+}
+
+export default getListStudents;
