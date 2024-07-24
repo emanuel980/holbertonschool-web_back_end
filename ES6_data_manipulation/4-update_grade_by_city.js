2c9becb6ccf3cab2b3bbe0e9ff3c98b89ecff564
Emanuel <emednedezdelgado22>
2024-07-23 23:01:41 -0400

    added files

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,30 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    if (!Array.isArray(newGrades)) {
+      return [];
+    }
+  
+    const studentsByCity = students.filter((student) => student.location === city);
+  
+    const studentsGraded = studentsByCity.map((student) => {
+      const gradeFilter = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+  
+      let grade;
+      if (gradeFilter[0]) {
+        grade = gradeFilter[0].grade;
+      } else {
+        grade = 'N/A';
+      }
+  
+      return {
+        ...student,
+        grade,
+      };
+    });
+  
+    return studentsGraded;
+  }
+  
+  export default updateStudentGradeByCity;
