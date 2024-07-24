edeea2e993c1c245a3d8bdc11a3e8919ef2e5e22
Emanuel <emednedezdelgado22>
2024-07-23 23:13:52 -0400

    changed file

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,14 @@
 function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
+  const view = new DataView(buffer);
 
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
 
-  view[position] = value;
+  view.setInt8(position, value);
 
-  return view;
+  return new Int8Array(buffer);
 }
 
 export default createInt8TypedArray;
